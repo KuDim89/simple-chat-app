642bd788c5ce2290e57432be01766a6b8d6e51b3
KuDim89 <36006135+KuDim89@users.noreply.github.com>
2021-09-22 18:48:09 +0300

    was added Profile component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import './App.css';
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
+import Profile from "./components/Profile";
 
 const App = () => {
   return (
     <div className='app-wrapper'>
       <Header/>
       <Navbar />
-      <div className='content'>
-        <img className='content-header' src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg" alt=""/>
-        <div>
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVkOdUDfgnkJ6pg25hxZboJY9V62vC8FB92g&usqp=CAU" alt=""/>
-          +desc
-        </div>
-        <div>My posts</div>
-        <div>New posts</div>
-        <div>Post 1</div>
-        <div>Post 2</div>
-      </div>
+      <Profile />
     </div>
   );
 }
---
+++ src/components/Profile.jsx
@@ -0,0 +1,19 @@
+import React  from "react"
+
+const Profile = () => {
+  return (
+      <div className='content'>
+        <img className='content-header' src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg" alt=""/>
+        <div>
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVkOdUDfgnkJ6pg25hxZboJY9V62vC8FB92g&usqp=CAU" alt=""/>
+          +desc
+        </div>
+        <div>My posts</div>
+        <div>New posts</div>
+        <div>Post 1</div>
+        <div>Post 2</div>
+      </div>
+  )
+}
+
+export default Profile;
