e7f0fd896ab9e01bef7288fda3d9e4173dd155d5
KuDim89 <36006135+KuDim89@users.noreply.github.com>
2021-09-22 18:31:22 +0300

    was added structure home page, all in one file.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 
 const App = () => {
   return (
-    <div className="App">Simple Chat</div>
+    <div className='app-wrapper'>
+      <header className='header'>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRePuK0LtkQi-nv6Py1ULm87rubc9yR9Z5n3Q&usqp=CAU" alt=""/>
+      </header>
+      <nav className='nav'>
+        <ul>
+          <li><a href="#">Profile</a></li>
+          <li><a href="#">Messages</a></li>
+          <li><a href="#">News</a></li>
+          <li><a href="#">Music</a></li>
+          <li><a href="#">Settings</a></li>
+        </ul>
+      </nav>
+      <div className='content'>
+        <img className='content-header' src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg" alt=""/>
+        <div>
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVkOdUDfgnkJ6pg25hxZboJY9V62vC8FB92g&usqp=CAU" alt=""/>
+          +desc
+        </div>
+        <div>My posts</div>
+        <div>New posts</div>
+        <div>Post 1</div>
+        <div>Post 2</div>
+      </div>
+    </div>
   );
 }
 
