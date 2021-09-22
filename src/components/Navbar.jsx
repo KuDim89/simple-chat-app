8a2e4f30b467cab2a0343d9145ea53c13b05279f
KuDim89 <36006135+KuDim89@users.noreply.github.com>
2021-09-22 19:11:46 +0300

    was added styles for Header component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,11 +4,11 @@ const Navbar = () => {
   return (
       <nav className='nav'>
         <ul>
-          <li><a href="#">Profile</a></li>
-          <li><a href="#">Messages</a></li>
-          <li><a href="#">News</a></li>
-          <li><a href="#">Music</a></li>
-          <li><a href="#">Settings</a></li>
+          <li className='item'><a href="#">Profile</a></li>
+          <li className='item'><a href="#">Messages</a></li>
+          <li className='item'><a href="#">News</a></li>
+          <li className='item'><a href="#">Music</a></li>
+          <li className='item'><a href="#">Settings</a></li>
         </ul>
       </nav>
   )
