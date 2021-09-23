53936caf2994b9c389f14ba692053af390d0fcef
KuDim89 <36006135+KuDim89@users.noreply.github.com>
2021-09-23 10:36:37 +0300

    was added Post component

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,5 +1,6 @@
 import React  from "react"
 import classes from "./MyPosts.module.css"
+import Post from "./Post/Post";
 
 const MyPosts = () => {
   return (
@@ -9,18 +10,9 @@ const MyPosts = () => {
           <button>Add post</button>
         </div>
         <div className={classes.posts}>
-          <div className={classes.item}>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVkOdUDfgnkJ6pg25hxZboJY9V62vC8FB92g&usqp=CAU" alt=""/>
-            post 1
-          </div>
-          <div className={classes.item}>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVkOdUDfgnkJ6pg25hxZboJY9V62vC8FB92g&usqp=CAU" alt=""/>
-            post 2
-          </div>
-          <div className={classes.item}>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVkOdUDfgnkJ6pg25hxZboJY9V62vC8FB92g&usqp=CAU" alt=""/>
-            post 3
-          </div>
+          <Post />
+          <Post />
+          <Post />
         </div>
       </div>
   )
