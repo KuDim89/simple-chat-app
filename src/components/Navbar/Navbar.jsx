5bc6d55bedcef95b0115613e59952f06ed360eb6
KuDim89 <36006135+KuDim89@users.noreply.github.com>
2021-09-23 15:18:43 +0300

    was added react router dom dependencies

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,8 +5,8 @@ const Navbar = () => {
   return (
       <nav className={classes.nav}>
         <ul>
-          <li className={classes.item}><a href="#s">Profile</a></li>
-          <li className={`${classes.item} ${classes.active}`}><a href="#s">Messages</a></li>
+          <li className={classes.item}><a href="/profile">Profile</a></li>
+          <li className={`${classes.item} ${classes.active}`}><a href="/dialogs">Messages</a></li>
           <li className={classes.item}><a href="#s">News</a></li>
           <li className={classes.item}><a href="#s">Music</a></li>
           <li className={classes.item}><a href="#s">Settings</a></li>
