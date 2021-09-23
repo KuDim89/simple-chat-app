da7bf7cd9789b19ace91e42f3cff8d4359d2d3a8
KuDim89 <36006135+KuDim89@users.noreply.github.com>
2021-09-23 09:56:36 +0300

    was added active class to the Navbar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
       <nav className={classes.nav}>
         <ul>
           <li className={classes.item}><a href="#">Profile</a></li>
-          <li className={classes.item}><a href="#">Messages</a></li>
+          <li className={`${classes.item} ${classes.active}`}><a href="#">Messages</a></li>
           <li className={classes.item}><a href="#">News</a></li>
           <li className={classes.item}><a href="#">Music</a></li>
           <li className={classes.item}><a href="#">Settings</a></li>
